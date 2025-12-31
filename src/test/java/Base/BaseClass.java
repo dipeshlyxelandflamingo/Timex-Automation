5eb94c79ba3db11d5cd175b3c9cfc82c0fa6b4c9
dipeshlyxelandflamingo <dipesh.singh@lyxelandflamingo.com>
2025-12-31 15:24:22 +0530

    headless mode

--- src/test/java/Base/BaseClass.java
+++ src/test/java/Base/BaseClass.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -24,9 +25,29 @@ public class BaseClass {
 
 	    @BeforeClass
 	    public void setUpClass() {
+	    	
+	    	  WebDriverManager.chromedriver().setup();
 
-	        WebDriverManager.chromedriver().setup();
-	        driver = new ChromeDriver();
+	          ChromeOptions options = new ChromeOptions();
+	          options.addArguments("--disable-blink-features=AutomationControlled");
+	          options.addArguments("start-maximized");
+	          options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.7444.134 Safari/537.36");
+
+	          if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+	              options.addArguments("--headless=new");
+	              options.addArguments("--no-sandbox");
+	              options.addArguments("--disable-dev-shm-usage");
+	              options.addArguments("--disable-gpu");
+	              options.addArguments("--window-size=1920,1080");
+	          }
+
+	          options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+	          options.setExperimentalOption("useAutomationExtension", false);
+	          options.addArguments("--remote-allow-origins=*");
+
+	          driver = new ChromeDriver(options);
+
+	       
 
 	        driver.manage().window().maximize();
 	        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
